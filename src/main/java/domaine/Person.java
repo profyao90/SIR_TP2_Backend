f5801bea3287a5e073eb795789e01d273f2045ce
Wilfried <kalowilfried@gmail.com>
2018-03-01 00:22:59 +0100

    person

---
+++ src/main/java/domaine/Person.java
@@ -0,0 +1,76 @@
+package domaine;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Person{
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String prenom;
+	private String mail;
+
+	@OneToMany(mappedBy = "personne", cascade = CascadeType.PERSIST)
+	private Collection<Residence> residences;
+
+	public Person() {
+
+	}
+
+	public Person(long id, String name, String prenom, String mail, Collection<Residence> residences) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.residences = residences;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Collection<Residence> getResidences() {
+		return residences;
+	}
+
+	public void setResidences(Collection<Residence> residences) {
+		this.residences = residences;
+	}
+
+}
